ca6a019d18d0c22899321edb92a92488a4cc7992
Tom Kiesow <github@kiesow.email>
2025-07-31 20:10:56 +0200

    fix comment, add trailing newline

--- pewpew/src/user_settings.rs
+++ pewpew/src/user_settings.rs
@@ -1,4 +1,4 @@
-// this file is in .gitignore, changes to the values in this file should not be committed
+// changes to the values in this file should not be committed
 
 // set the path to the serial port here
 // e.g. Some("/dev/ttyoAss") or Some("COM69420")
