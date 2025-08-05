9f7eed18007778f44134153fb4be5b49634d0122
tfki <github@kiesow.email>
2025-08-05 14:05:42 +0200

    cancel cancel_token on drop (#6)
    * cancel cancel_token on drop
    
    * use default instead of new

--- pewpew/src/cancel_token.rs
+++ pewpew/src/cancel_token.rs
@@ -1,20 +1,29 @@
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 
+#[derive(Clone)]
 pub struct CancelToken {
-    canceled: AtomicBool,
+    canceled: Arc<AtomicBool>,
 }
 
 impl CancelToken {
-    pub fn new() -> Arc<Self> {
-        Arc::new(Self { canceled: AtomicBool::new(false)})
-    }
-
-    pub fn cancel(self: Arc<Self>) {
+    pub fn cancel(&self) {
         self.canceled.store(true, Ordering::Relaxed);
     }
 
     pub fn was_canceled(&self) -> bool {
         self.canceled.load(Ordering::Relaxed)
+    }
+}
+
+impl Default for CancelToken {
+    fn default() -> Self {
+        CancelToken { canceled: Arc::new(AtomicBool::new(false)) }
+    }
+}
+
+impl Drop for CancelToken {
+    fn drop(&mut self) {
+        self.cancel();
     }
 }
