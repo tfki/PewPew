bae4294a984c0e4518e31ace5e78a6a8680c2fae
Sebastian Balint <S.Balint@web.de>
2025-08-07 16:19:08 +0200

    Added GUI for IM0 that does nothing with Message content

--- pewpew/src/main.rs
+++ pewpew/src/main.rs
@@ -3,8 +3,16 @@ use pewpew::cancel_token::CancelToken;
 use pewpew::serial::config::SerialConfig;
 use pewpew::serial::packet::Packet;
 use pewpew::serial::reader::SerialReader;
-use std::sync::mpsc::{channel, Receiver, Sender};
+use std::alloc::System;
+use std::sync::mpsc::{Receiver, Sender, channel};
 use std::thread;
+
+use sdl2::event::Event;
+use sdl2::image::{InitFlag, LoadTexture};
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::thread;
+use std::time::{Duration, SystemTime};
 
 fn run_serial(sender: Sender<Packet>, cancel_token: CancelToken) -> impl FnOnce() {
     // this function does not run the code below
@@ -53,13 +61,73 @@ fn run_serial(sender: Sender<Packet>, cancel_token: CancelToken) -> impl FnOnce(
     }
 }
 
-fn run_gui(_receiver: Receiver<Packet>, cancel_token: CancelToken) {
-    // TODO
-    loop {
+// TODO replace Packet in Receiver<Packet> with actual message type of Reload- and Shotmessages.
+fn run_gui(receiver: Receiver<Packet>, cancel_token: CancelToken) {
+    let sdl_context = sdl2::init().unwrap();
+
+    let video = sdl_context.video().unwrap();
+    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG).unwrap();
+    let refresh_rate = video.desktop_display_mode(0).unwrap().refresh_rate;
+    let screen_width = video.desktop_display_mode(0).unwrap().w;
+    let screen_height = video.desktop_display_mode(0).unwrap().h;
+
+    let window = video
+        .window(
+            "PewPew sdl-sandbox",
+            screen_width as u32,
+            screen_height as u32,
+        )
+        .fullscreen_desktop()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())
+        .unwrap();
+
+    let mut canvas = window
+        .into_canvas()
+        .present_vsync() //< this means the screen cannot
+        // render faster than your display rate (usually 60Hz or 144Hz)
+        // .software()
+        .build()
+        .map_err(|e| e.to_string())
+        .unwrap();
+
+    canvas.set_draw_color(Color::BLACK);
+    canvas.clear();
+    canvas.present();
+    let mut last_present = SystemTime::now();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'gui_running: loop {
         if cancel_token.was_canceled() {
             info!(target: "Gui Thread", "exiting because of cancel token");
             return;
         }
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'gui_running,
+                _ => {}
+            }
+        }
+
+        if SystemTime::now().duration_since(last_present).unwrap()
+            > Duration::from_secs_f64(1.0 / refresh_rate as f64)
+        {
+            if let Ok(_recv) = receiver.try_recv() {
+                canvas.set_draw_color(Color::WHITE);
+                // do something with recv
+            } else {
+                canvas.set_draw_color(Color::BLACK);
+            }
+            canvas.clear();
+            canvas.present();
+            last_present = SystemTime::now();
+        }
+        thread::sleep(Duration::from_millis(1));
     }
 }
 
